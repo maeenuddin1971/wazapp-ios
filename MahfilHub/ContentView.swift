54fb7ace7b23d650888e91b1bf4ac5b8d2682708
Bipul Islam <bipulhstu@gmail.com>
2026-03-07 17:15:20 +0600

    Initial Commit

---
+++ MahfilHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MahfilHub
+//
+//  Created by Bipul Islam on 7/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MahfilHub/MahfilHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MahfilHubApp.swift
+//  MahfilHub
+//
+//  Created by Bipul Islam on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MahfilHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
